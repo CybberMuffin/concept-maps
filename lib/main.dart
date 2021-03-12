fdcc369b02cb9ef4630ac6868b188175e09a5c11
Dmytro Kovalenko <dkovalneko@varteq.com>
2021-03-12 14:42:51 +0200

    fix valya's swipedetector

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:concept_maps/views/BottomPannel.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:concept_maps/views/BottomMenu.dart';
+import 'views/CourseMain.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,12 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My Demo Media Query",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.montserratTextTheme(
           Theme.of(context).textTheme,
         ),
       ),
-      home: BottomMenu(),
+      home: CourseMain(), //change to BottomMenu() here if u need to
     );
   }
 }
