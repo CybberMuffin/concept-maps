2d8db48e0ac5f0f4dd26aaf0901a118518f2d42d
valentyna <valyakyo@gmail.com>
2022-11-17 19:27:58 +0200

    course statistics layout

--- lib/views/statistics/statistics_screen.dart
+++ lib/views/statistics/statistics_screen.dart
@@ -5,6 +5,7 @@ import 'package:concept_maps/utils/app_colors.dart';
 import 'package:concept_maps/utils/date_time_formatter.dart';
 import 'package:concept_maps/views/widgets/texts/main_text.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 import 'package:provider/provider.dart';
 
@@ -41,39 +42,14 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
                 thickness: 4,
                 child: SingleChildScrollView(
                   child: Padding(
-                    padding: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
                     child: Column(
                       children: List.generate(
                         conceptNames.length,
-                        (index) => Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 8, horizontal: 16),
-                          margin: const EdgeInsets.only(bottom: 8),
-                          decoration: BoxDecoration(
-                            color: Colors.blue.withOpacity(0.3),
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  conceptNames[index],
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 8,
-                              ),
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(
-                                  DateTimeFormatter.getFormattedTime(
-                                    conceptTimes[index],
-                                    true,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                        (index) => statisticsTile(
+                          conceptNames[index],
+                          conceptTimes[index],
                         ),
                       ),
                     ),
@@ -97,4 +73,45 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
           },
         ),
       );
+
+  Widget statisticsTile(String title, int time) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.blue.withOpacity(0.3),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              title,
+              style: GoogleFonts.montserrat(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              DateTimeFormatter.getFormattedTime(
+                time,
+                true,
+              ),
+              style: GoogleFonts.montserrat(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
